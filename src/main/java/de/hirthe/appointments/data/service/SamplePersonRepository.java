e16b48d16616526e40983818604da939ae58946b
start.vaadin.com <no-reply@vaadin.com>
2022-06-16 05:13:05 +0000

    Generated project

---
+++ src/main/java/de/hirthe/appointments/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package de.hirthe.appointments.data.service;
+
+import de.hirthe.appointments.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
---
+++ src/main/java/de/hirthe/appointments/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package de.hirthe.appointments.data.service;
+
+import de.hirthe.appointments.data.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+}
